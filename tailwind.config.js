7ffd2d3accc5b5fdfc13d4c659022a6a7029e83a
Julie <juptsr.becode@gmail.com>
2022-04-27 18:51:31 +0200

    responsive mobile ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        boletta: ["bellotaregular", "cursive"],
-        bolettabold: ["bellotabold", "cursive"],
-      },
-      margin: {
-        'm-y-form': '80px',
-      },
-      padding: {
-        'p-x': '40px',
-      }
+      
     },
   },
   plugins: [
